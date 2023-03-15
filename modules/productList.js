523d8cb43c656296f84e133afada08c44f4b8238
HeeSoon <phs970824@daum.net>
2023-03-15 14:08:20 +0900

    productList 컴포넌트 구현 완료

--- modules/productList.js
+++ modules/productList.js
@@ -2,29 +2,29 @@ import { appendChildrenList, makeDOMwithProperties } from "../utils/dom.js";
 import { productCard } from "./productCard.js";
 
 export const productList = (bookInfo, sectionInfo) => {
-  {/* <div class="section-title best-seller">
-          <h2 class="sec-title">신규or실시간 인기작</h2>
-          <div class="book-component">
-            <ul class="prod-list">
-              <li class="prod-card">
-                <div class="book-poster">
-                  <img src="/img/myfirstmemory.jpg" alt="myfirstmemory">
-                </div>
-                <div class="book-info">
-                  <h3 class="book-name">My first memory</h3>
-                  <div class="price"> 1화 n원 </div>
-                  <span>작가명</span>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div> */}
-const sectionTitle = makeDOMwithProperties('div', {
+  {/* <div class="section-list best-seller">
+        <h2 class="section-title">신규or실시간 인기작</h2>
+        <div class="book-component">
+          <ul class="prod-list">
+            <li class="prod-card">
+              <div class="book-poster">
+                <img src="/img/myfirstmemory.jpg" alt="myfirstmemory">
+              </div>
+              <div class="book-info">
+                <h3 class="book-name">My first memory</h3>
+                <div class="price"> 1화 n원 </div>
+                <span>작가명</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div> */}
+const sectionList = makeDOMwithProperties('div', {
   className: sectionInfo.classname,
-  className: 'section-title'
+  className: 'section-list'
 })
-const secTitle = makeDOMwithProperties('h2', {
-  className: 'sec-title',
+const sectionTitle = makeDOMwithProperties('h2', {
+  className: 'section-title',
   innerText: sectionInfo.title
 })
 const bookComponent = makeDOMwithProperties('div', {
@@ -34,18 +34,22 @@ const prodList = makeDOMwithProperties('ul', {
   className: 'prod-list'
 });
 
+console.log(bookInfo[sectionInfo.classname])
 
-// const prodCard = productCard(bookInfo.best[0]);
-// console.log(prodCard)
-bookInfo.info.classname.forEach((prodInfo) => {
+
+bookInfo[sectionInfo.classname].forEach((prodInfo) => {
   const prodCard = productCard(prodInfo);
   appendChildrenList(prodList,[prodCard]);
 });
+// bookInfo.info.classname.forEach((prodInfo) => {
+//   const prodCard = productCard(prodInfo);
+//   appendChildrenList(prodList,[prodCard]);
+// });
 
 appendChildrenList(bookComponent,[prodList]);
-appendChildrenList(sectionTitle,[secTitle, bookComponent]);
+appendChildrenList(sectionList,[sectionTitle, bookComponent]);
 
 const container = document.getElementsByClassName('container')[0];
-appendChildrenList(container, [sectionTitle]);
+appendChildrenList(container, [sectionList]);
 
 }
